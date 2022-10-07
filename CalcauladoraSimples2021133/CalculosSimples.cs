d50fd3ad3b0dc8dc9cc5a8bbda8ebeb450fcab7a
c-Campos-ss <caua.campos310506@gmail.com>
2022-10-07 16:31:50 -0300

    Programas alterados e incluidos a multiplicação e divisão.

--- CalcauladoraSimples2021133/CalculosSimples.cs
+++ CalcauladoraSimples2021133/CalculosSimples.cs
@@ -17,5 +17,18 @@ public class CalculosSimples
         return subtracao;
 
     }
-    
+
+    public double Dividir(double numero1, double numero2)
+    {
+        double divisao = numero1 / numero2;
+        return divisao;
+
+    }
+    public double Multiplicar(double numero1, double numero2)
+    {
+        double multiplicacao = numero1 * numero2;
+        return multiplicacao;
+
+    }
+
 }
